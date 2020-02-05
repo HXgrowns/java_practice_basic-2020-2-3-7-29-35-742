2391f0e4213320598cf361c8a3032a5b24443e75
huxiao <804156017@qq.com>
2020-02-05 12:23:50 +0800

    [huxiao] {add}: add array code

--- 001_array/src/main/java/com/thoughtworks/ArrayPractice1.java
+++ 001_array/src/main/java/com/thoughtworks/ArrayPractice1.java
@@ -1,16 +1,21 @@
 package com.thoughtworks;
 
+import java.util.Arrays;
+
 public class ArrayPractice1 {
 
     public static void main(String[] args) {
         printArrayReverse();
     }
-
-    /**
-     * 倒序打印给定数组,打印格式为: [3,2,1]
-     */
     public static void printArrayReverse() {
         int[] array = new int[]{1, 2, 3};
 
+        for (int i = 0; i < array.length / 2; i++) {
+            int t = array[i];
+            array[i] = array[array.length-1-i];
+            array[array.length-1-i] = t;
+        }
+
+        System.out.println(Arrays.toString(array));
     }
 }
--- 001_array/src/main/java/com/thoughtworks/ArrayPractice4.java
+++ 001_array/src/main/java/com/thoughtworks/ArrayPractice4.java
@@ -1,13 +1,27 @@
 package com.thoughtworks;
 
-public class ArrayPractice4 {
+import java.util.ArrayList;
 
-    /**
-     * 完成本方法实现功能: 将一个数字插入到给定的已经排好序的数组中，要求插入以后数组依然是有序的
-     */
+public class ArrayPractice4 {
     public static int[] insert(int number) {
         int[] array = new int[]{1, 20, 50, 100};
+        ArrayList<Integer> newArray = new ArrayList<Integer>();
 
-        return null;
+        boolean flag = true;
+        for (int i = 0; i < array.length; i++) {
+            if(flag && number <= array[i]) {
+                newArray.add(number);
+                flag = false;
+            }
+            newArray.add(array[i]);
+        }
+
+        if(flag) {
+            newArray.add(number);
+        }
+
+        int[] result = newArray.stream().mapToInt(Integer::valueOf).toArray();
+
+        return result;
     }
 }
